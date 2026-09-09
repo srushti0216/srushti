bc8b40199870bc976ae91ab897cec81ba9afb3c0
Tushar Patil <srushti.tushar-patil@capgemini.com>
2026-09-09 16:18:53 +0530

    'v1'

---
+++ Cap2.py
@@ -0,0 +1,146 @@
+# # # # a=['apple','grapes','oranges','guava']
+# # # # c=['straw','berry','kiwi','fruit']
+# # # # d=a+c
+# # # # print(type(a))
+# # # # b=a[1:3]
+# # # # c[3]='hj'
+# # # # d[3:5]=['lk','gg']
+# # # # print(d)
+
+# # # # x=['car','bike','plane','tempo']
+# # # # y=['boat','truck','ship','cart']
+# # # # z=x+y
+# # # # print(type(x))
+# # # # u=x[1:3]
+# # # # y[2]='cargo'
+# # # # z[2:4]=['aeroplane','scooty']
+# # # # print(z)
+
+# # # #thislist = ["apple", "banana", "cherry"]
+# # # #thislist.append("orange")
+
+# # # #thislist.insert(1, "orange")
+
+# # # #thislist.remove("banana")
+# # # #thislist.pop(1)
+
+# # # # thislist = [100, 50, 65, 82, 23]
+# # # # thislist.sort(reverse = True)
+
+# # # # print(thislist)
+
+# # # # thistuple = ("apple", "banana", "cherry")
+# # # # #print(type(thistuple))
+# # # # thistuple[1]='k1'
+# # # # print(thistuple)
+
+# # # # x = ("apple", "banana", "cherry")
+# # # # y = list(x)
+# # # # y[1] = "kiwi"
+# # # # x = tuple(y)
+
+# # # # print(x)
+
+# # # # fruits = ("apple", "banana", "cherry")
+# # # # (green, yellow, red) = fruits
+# # # # print(green)
+# # # # print(yellow)
+# # # # print(red)
+
+# # # thisset = {"apple", "banana", "cherry"}
+# # # thisset.remove("banana")
+# # # print(thisset)
+
+# # # thisset = {"apple", "banana", "cherry"}
+# # # thisset.discard("banana")
+# # # print(thisset)
+
+# # # thistuple = ("apple", "banana", "cherry")
+# # # y = list(thistuple)
+# # # y.append("orange")
+# # # thistuple = tuple(y)
+# # # print(y)
+
+# # # thistuple = ("apple", "banana", "cherry")
+# # # y = list(thistuple)
+# # # y.remove("apple")
+# # # thistuple = tuple(y)
+# # # print(y)
+
+# # # fruits = ("apple", "banana", "cherry")
+# # # mytuple = fruits * 2
+# # # print(mytuple)
+
+# # # thisset = {"apple", "banana", "cherry", True, 1, 2}
+# # # print(thisset)
+
+# # set1 = {"apple", "banana", "cherry"}
+# # set2 = {1, 5, 7, 9, 3}
+# # set3 = {True, False, False}
+# # print(type(set2))
+
+# # thisset = {"apple", "banana", "cherry"}
+# # tropical = {"pineapple", "mango", "papaya"}
+
+# # thisset.remove('banana')
+
+# # print(thisset)
+
+# # set1 = {"a", "b", "c"}
+# # set2 = {1, 2, 3}
+
+# # set3 = set1.union(set2)
+# # print(set3)
+
+# # thisdict = {
+# #   "brand": "Ford",
+# #   "model": "Mustang",
+# #   "year": 1964
+# # }
+# # print(thisdict["brand"])
+
+# car = {
+# "brand": "Ford",
+# "model": "Mustang",
+# "year": 1964
+# }
+
+# car = {
+# "brand": "Ford",
+# "model": "Mustang",
+# "year": 1964
+# }
+
+# x = car.keys()
+# print(x) #before the change
+# car["color"] = "white"
+# print(x) #after the change
+
+# x = car.values()
+# print(x) #before the change
+# car["color"] = "red"
+# print(x) #after the change
+
+#thisdict = dict(name = "John", age = 36, country = "Norway")
+#print(thisdict)
+
+# fruits = ["apple", "banana", "cherry"]
+# for x in fruits:
+#   print(x)
+
+# for x in range(6):
+#   print(x)
+
+# for x in range(2, 6):
+#   print(x)
+
+# for x in range(2, 30, 3):
+#   print(x)
+
+# adj = ["red", "big", "tasty"]
+# fruits = ["apple", "banana", "cherry"]
+
+# for x in adj:
+#   for y in fruits:
+#     print(x,y)
+
---
+++ index.py
@@ -0,0 +1,98 @@
+print('Hello everyone')
+print('Have a good day')
+#This is a sample code
+print('Learning Python is fun')
+print('''Hi all
+123''')
+Vs='Srushti'
+bS='Patil'
+print(Vs+' '+bS)
+x, y, z = "Orange", "Banana", "Cherry"
+print(x)
+print(y)
+print(z)
+x = 5
+y = 10
+print(x + y)
+x = 'Srushti'
+y = 719
+print('name:',x, 'id:', y)
+
+a=100
+b=4200
+
+if a>b:
+    print('a is less than b')
+elif b>a:
+    print('b')
+else:
+    print('yeah')
+
+a=24
+if a>18:
+    print('Eligible for voter card')
+else:
+    print('minor')
+
+x = 2
+y = 65
+print(x * y)
+
+x = 300
+y = 240
+print(x-y)
+
+x = 52
+if 85<x<100:
+    print('Grade A')
+elif 75<x<85:
+    print('Grade B')
+elif 65<x<75:
+    print('Grade C')
+else:
+    print('Fail')
+
+x = 96
+if 85<x<100:
+    print('Grade A')
+elif 75<x<85:
+    print('Grade B')
+elif 65<x<75:
+    print('Grade C')
+else:
+    print('Fail')
+
+b=int(input('Enter your first number:'))
+c=int(input('Enter your second number:'))
+p=b+c
+
+print(p)
+
+p=int(input('Enter your age:'))
+if p>18:
+    print('Eligible for voter card')
+else:
+    print('minor')
+
+x=int(input('Enter your first number:'))
+y=int(input('Enter your second number:'))
+p=x*y
+
+print(p)
+
+p=int(input('Enter your Marks:'))
+if 85<p<100:
+    print('Grade A')
+elif 75<p<85:
+    print('Grade B')
+elif 65<p<75:
+    print('Grade C')
+else:
+    print('Fail')
+
+x=float(input('Enter your first number:'))
+y=float(input('Enter your second number:'))
+z=float(input('Enter your third number:'))
+p=(x+y-z/y)
+
+print(p)
