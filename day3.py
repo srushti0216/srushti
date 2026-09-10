37b4de2c5b5e79a461946e34fcbc20dbd43fcbf4
Tushar Patil <srushti.tushar-patil@capgemini.com>
2026-09-10 11:23:01 +0530

    'v1'

--- day3.py
+++ day3.py
@@ -1,5 +1,60 @@
-def my_function():
-  print("Hello from a function")
+# def my_function():
+#   print("Hello from a function")
 
-my_function()
+# my_function()
 
+# def my_function(fname):
+#   print(fname + " Refsnes")
+
+# my_function("Emil")
+# my_function("Tobias")
+# my_function("Linus")
+
+
+# def my_function(*kids):
+#   print("The youngest child is " + kids[1])
+
+# my_function("Emil", "Tobias", "Linus")
+
+# def calc():
+#   a=10
+#   b=20
+#   if a<b:
+#     print('a is less than b')
+#   else:
+#     print('a is greater than b')
+
+# calc()
+
+# def myfunc():
+#   x = 300
+#   print(x)
+
+# myfunc()
+
+# def changecase(func):
+#   def myinner():
+#     return func().upper()
+#   return myinner
+
+# @changecase
+# def myfunction():
+#   return "Hello Sally"
+
+# print(myfunction())
+
+def changecase(func):
+  def myinner():
+    return func().upper()
+  return myinner
+
+@changecase
+def myfunction():
+  return "Hello Sally"
+
+@changecase
+def otherfunction():
+  return "I am speed!"
+
+print(myfunction())
+print(otherfunction())
