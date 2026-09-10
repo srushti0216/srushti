a141533a457a778fdb2c58292b049a06f7d5b327
Tushar Patil <srushti.tushar-patil@capgemini.com>
2026-09-10 10:18:30 +0530

    'v1'

--- day2.py
+++ day2.py
@@ -1,2 +1,22 @@
-x=('20','85','79','90','100','77','88','45','86','76')
+x=[20,80,38,55,98,56,82,76,56,34]
+average_marks= sum(x)/len(x)
 print(x)
+print(max(x))
+print(min(x))
+print('average marks:', average_marks)
+
+n=[10, 20, 30, 40, 50]
+n.append(60)
+print(n)
+
+n=[10, 20, 30, 40, 50]
+n.remove(20)
+print(n)
+
+n=[10, 20, 30, 40, 50]
+n.insert(1,15)
+print(n)
+
+n=[10, 20, 30, 40, 50]
+n.reverse()
+print(n)
